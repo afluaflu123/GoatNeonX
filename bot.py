4bb64237087650dd6908d855e37e5c2d9f822c00
HacKer Jr ~ TG <83758692+afluaflu123@users.noreply.github.com>
2023-08-24 21:19:27 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,6 @@ logging.getLogger().setLevel(logging.INFO)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("imdbpy").setLevel(logging.ERROR)
 
-
 from pyrogram import Client, __version__, filters
 from pyrogram.raw.all import layer
 from database.ia_filterdb import Media
@@ -17,10 +16,11 @@ from utils import temp
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
 from Script import script 
+from datetime import date, datetime
 from aiohttp import web
 from plugins import web_server
-from datetime import date, datetime 
-import pytz
+
+PORT = "8080"
 
 class Bot(Client):
 
