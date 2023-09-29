83dfbf21b861641f2a32635b1322f1555bcb8d2b
HacKer Jr ~ TG <83758692+afluaflu123@users.noreply.github.com>
2023-09-29 17:34:01 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -275,7 +275,7 @@ async def start(client, message):
         InlineKeyboardButton('⚠️ Dᴇʟᴇᴛᴇ Fɪʟᴇ ⚠️', callback_data='close_data')
     ]]    
     k = await msg.reply("<b><u>❗️❗️IMPORTANT❗️️❗️</u>\n\n⚠️ This File Will Be Deleted From Here Within <u>10 Minute</u>...!\n\nPlease Forward This File To Your Saved Messages And Start Download There ☺️.</b>",quote=True)
-    await asyncio.sleep(200)
+    await asyncio.sleep(80)
     await msg.delete()
     await k.edit_text("<b><i>• ʏᴏᴜʀ ꜰɪʟᴇ ɪs sᴜᴄᴄᴇssꜰᴜʟʟʏ ᴅᴇʟᴇᴛᴇᴅ.</i></b>",reply_markup=InlineKeyboardMarkup(btn))
     await message.delete()
